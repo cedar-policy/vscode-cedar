8184081dbc48c5bafef94db4473565b883bedfc0
Kevin Hakanson <kevhak@amazon.com>
2024-04-14 16:56:16 -0500

    chore: update copyright
    Signed-off-by: Kevin Hakanson <kevhak@amazon.com>

--- src/help.ts
+++ src/help.ts
@@ -1,4 +1,4 @@
-// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// Copyright Cedar Contributors
 // SPDX-License-Identifier: Apache-2.0
 
 export const FUNCTION_HELP_DEFINITIONS: Record<string, string[]> = {
