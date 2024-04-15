d1e6d47af2fd7fc9e649f176d854ec71f701f4af
Kevin Hakanson <kevhak@amazon.com>
2024-04-14 20:16:32 -0500

    chore: test cleanup
    Signed-off-by: Kevin Hakanson <kevhak@amazon.com>

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -2,9 +2,6 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import * as assert from 'assert';
-
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
 import * as vscode from 'vscode';
 import * as path from 'path';
 import * as completion from '../../completion';
