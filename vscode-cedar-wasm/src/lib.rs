daf9691e6d7b0ad739cf068afa955fb2c6616ee3
Kevin Hakanson <kevhak@amazon.com>
2024-04-26 09:30:41 -0500

    chore: cargo fmt
    Signed-off-by: Kevin Hakanson <kevhak@amazon.com>

--- vscode-cedar-wasm/src/lib.rs
+++ vscode-cedar-wasm/src/lib.rs
@@ -1,11 +1,11 @@
 // Copyright Cedar Contributors
 // SPDX-License-Identifier: Apache-2.0
 
-mod utils;
-mod format;
-mod syntax_validator;
-mod policy_validator;
-mod schema_validator;
 mod entities_validator;
+mod format;
 mod policy;
+mod policy_validator;
 mod schema_translate;
+mod schema_validator;
+mod syntax_validator;
+mod utils;
--- vscode-cedar-wasm/src/policy.rs
+++ vscode-cedar-wasm/src/policy.rs
@@ -23,44 +23,44 @@ pub struct ExportPolicyResult {
 
 #[wasm_bindgen(js_name = exportPolicy)]
 pub fn export_policy(input_policy_str: &str) -> ExportPolicyResult {
-  let Ok(policy) = Policy::parse(None, input_policy_str) else {
-      return ExportPolicyResult {
-        success: false,
-        json: None,
-      };
-  };
+    let Ok(policy) = Policy::parse(None, input_policy_str) else {
+        return ExportPolicyResult {
+            success: false,
+            json: None,
+        };
+    };
 
-  let Ok(json) = policy.to_json() else {
-      return ExportPolicyResult {
-        success: false,
-        json: None,
-      };
-  };
+    let Ok(json) = policy.to_json() else {
+        return ExportPolicyResult {
+            success: false,
+            json: None,
+        };
+    };
 
-  ExportPolicyResult {
-    success: true,
-    json: Some(json.to_string()),
-  }
+    ExportPolicyResult {
+        success: true,
+        json: Some(json.to_string()),
+    }
 }
 
 #[wasm_bindgen(js_name = exportPolicyTemplate)]
 pub fn export_policy_template(input_policy_template_str: &str) -> ExportPolicyResult {
-  let Ok(policy) = Template::parse(None, input_policy_template_str) else {
-      return ExportPolicyResult {
-        success: false,
-        json: None,
-      };
-  };
+    let Ok(policy) = Template::parse(None, input_policy_template_str) else {
+        return ExportPolicyResult {
+            success: false,
+            json: None,
+        };
+    };
 
-  let Ok(json) = policy.to_json() else {
-      return ExportPolicyResult {
-        success: false,
-        json: None,
-      };
-  };
+    let Ok(json) = policy.to_json() else {
+        return ExportPolicyResult {
+            success: false,
+            json: None,
+        };
+    };
 
-  ExportPolicyResult {
-    success: true,
-    json: Some(json.to_string()),
-  }
+    ExportPolicyResult {
+        success: true,
+        json: Some(json.to_string()),
+    }
 }
